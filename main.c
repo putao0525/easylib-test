14123a7c03135cb00706a5db76eac9eeb8f1dd1d
luyao21 <putao@putao.local>
2024-09-02 18:07:54 +0800

    upate

--- main.c
+++ main.c
@@ -4,5 +4,6 @@
 int main(void) {
     struct tm *curTime = easy_get_current_time();
     EASY_LOG_INFO("当前时间:%s", easy_format_time(curTime, NORM_DATETIME_PATTERN));
+    EASY_DELETE(curTime);
     return 0;
 }
